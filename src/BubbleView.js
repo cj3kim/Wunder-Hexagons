85c7be88785520f7c02df43ab705ebfe6af387fc
Chris Kim <cj3kim@gmail.com>
2014-12-23 21:16:12 -0800

    Moved showBubbles back into BubbleView init function

--- src/BubbleView.js
+++ src/BubbleView.js
@@ -38,68 +38,69 @@ function BubbleView() {
     }
   });
 
+
+  this.showBubbles = function (jsonData) {
+    var defaultBubbleCount = 4
+    var imagesLength = jsonData.images.length
+    var lastIndex = defaultBubbleCount - 1
+
+    var topics = jsonData.topics;
+
+    function _showNext(index) {
+      if (index === -1) {
+        self._eventOutput.emit('finishedBubbling', jsonData);
+      } else {
+        var surface = bubbles[index].surface;
+        var rc = surface.renderController;
+        var nextIndex = index - 1;
+
+        surface.d3_svg.selectAll('defs').remove();
+        surface.d3_svg.selectAll('text').text('');
+
+        function getRandomArbitrary(min, max) { return Math.floor(Math.random() * (max - min) + min); }
+        var rgb = _.template('rgb(<%= r %>,<%= g %>,<%= b %>)')({
+            r: getRandomArbitrary(0,255)
+          , g: getRandomArbitrary(0,255)
+          , b: getRandomArbitrary(0,255)
+        });
+
+        surface.d3_svg.select('path').attr('fill', rgb);
+
+        if (topics) {
+          if (topics[index]) {
+            var words = topics[index].split(' ');
+
+            surface.d3_svg.select('text').text(words[0]);
+            surface.d3_svg.append('text')
+              .attr('x', 23)
+              .attr('y', 75)
+              .attr('font-size', '20px')
+              .attr('fill', 'white')
+              .attr('text-align', 'center')
+              .text(words[1])
+              ;
+          }
+        } else {
+          if (index < imagesLength) {
+             var imagePath = jsonData.images[index];
+             var imageFileName = imagePath.replace(/^.*[\\\/]/, '');
+
+             applyPattern(surface.d3_svg, imageFileName, imagePath, {
+               x:-10, y:0, width: 120, height: 120
+             });
+          }
+        }
+        rc.show(surface, {duration: 400}, _showNext.bind(this, nextIndex))
+      }
+    }
+    _showNext.bind(this, lastIndex)();
+  };
+
+
 };
 
 BubbleView.prototype = Object.create(View.prototype);
 BubbleView.prototype.constructor = View;
-
-BubbleView.prototype.showBubbles = function (jsonData) {
-  var defaultBubbleCount = 4
-  var imagesLength = jsonData.images.length
-  var lastIndex = defaultBubbleCount - 1
-
-  var topics = jsonData.topics;
-
-  function _showNext(index) {
-    if (index === -1) {
-      self._eventOutput.emit('finishedBubbling', jsonData);
-    } else {
-      var surface = bubbles[index].surface;
-      var rc = surface.renderController;
-      var nextIndex = index - 1;
-
-      surface.d3_svg.selectAll('defs').remove();
-      surface.d3_svg.selectAll('text').text('');
-
-      function getRandomArbitrary(min, max) { return Math.floor(Math.random() * (max - min) + min); }
-      var rgb = _.template('rgb(<%= r %>,<%= g %>,<%= b %>)')({
-          r: getRandomArbitrary(0,255)
-        , g: getRandomArbitrary(0,255)
-        , b: getRandomArbitrary(0,255)
-      });
-
-      surface.d3_svg.select('path').attr('fill', rgb);
-
-      if (topics) {
-        if (topics[index]) {
-          var words = topics[index].split(' ');
-
-          surface.d3_svg.select('text').text(words[0]);
-          surface.d3_svg.append('text')
-            .attr('x', 23)
-            .attr('y', 75)
-            .attr('font-size', '20px')
-            .attr('fill', 'white')
-            .attr('text-align', 'center')
-            .text(words[1])
-            ;
-        }
-      } else {
-        if (index < imagesLength) {
-           var imagePath = jsonData.images[index];
-           var imageFileName = imagePath.replace(/^.*[\\\/]/, '');
-
-           applyPattern(surface.d3_svg, imageFileName, imagePath, {
-             x:-10, y:0, width: 120, height: 120
-           });
-        }
-      }
-      rc.show(surface, {duration: 400}, _showNext.bind(this, nextIndex))
-    }
-  }
-  _showNext.bind(this, lastIndex)();
-};
-
 
 BubbleView.prototype.createBubble = function createBubble(coordinates, size, properties) {
   var x = coordinates[0];
